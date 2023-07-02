7a4e915b388f164b47284eeacfcd210756903b3e
Wizart24 <deivas24@gmail.com>
2023-07-02 17:46:59 +0300

    First Commit

---
+++ API/API/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using API.Data;
+using API.Dtos.User;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class AuthController : ControllerBase
+	{
+		private readonly IAuthRepository _authRepo;
+
+		public AuthController(IAuthRepository authRepo)
+        {
+			_authRepo = authRepo;
+		}
+
+		[HttpPost("Register")]
+		public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+		{
+			var response = await _authRepo.Register(
+				new User { Username = request.Username }, request.Password
+			);
+
+			if (!response.Success)
+			{
+				return BadRequest(response);
+			}
+
+			return Ok(response);
+		}
+
+		[HttpPost("Login")]
+		public async Task<ActionResult<ServiceResponse<int>>> Login (UserLoginDto request)
+		{
+			var response = await _authRepo.Login(request.Username, request.Password);
+
+			if (!response.Success)
+			{
+				return BadRequest(response);
+			}
+
+			return Ok(response);
+		}
+    }
+}
